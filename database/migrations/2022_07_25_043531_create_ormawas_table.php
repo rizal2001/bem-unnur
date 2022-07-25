c00d0616a381c32c1cfa2d0dc7c6f013171c0885
enuibnu <sofyanibnu24@gmail.com>
2022-07-25 11:40:53 +0700

    Add all Migrations & Models dari SchemaDB db_bem

---
+++ database/migrations/2022_07_25_043531_create_ormawas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_ormawa', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kategori_ormawa_id');
+            $table->string('nama');
+            $table->string('gambar_logo');
+            $table->timestamps();
+
+            $table->foreign('kategori_ormawa_id')->references('id')->on('m_kategori_ormawa');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ormawas');
+    }
+};
