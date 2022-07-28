686f7a48a81ae24fc35f046d5da558c9400dee69
enuibnu <sofyanibnu24@gmail.com>
2022-07-28 14:43:47 +0700

    Add Models & Migrations

---
+++ app/Models/Background.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Halaman;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Background extends Model
+{
+    protected $table = 't_background';
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+
+    public function background(){
+        return $this->belongsTo(Halaman::class);
+    }
+}
